b8351da44a8babe48832892cc50cfdf3fadb82b7
gua12345 <121714613+gua12345@users.noreply.github.com>
2026-01-04 15:43:47 +0800

    Revert "feat: 增加public的增加邮箱和删除邮箱功能"
    This reverts commit 9df05223fda0053fc7de73317e19e555ebffe1e9.

--- mail-worker/src/api/public-api.js
+++ mail-worker/src/api/public-api.js
@@ -1,7 +1,6 @@
 import app from '../hono/hono';
 import result from '../model/result';
 import publicService from '../service/public-service';
-import accountService from '../service/account-service';
 
 app.post('/public/genToken', async (c) => {
 	const data = await publicService.genToken(c, await c.req.json());
@@ -15,19 +14,5 @@ app.post('/public/emailList', async (c) => {
 
 app.post('/public/addUser', async (c) => {
 	await publicService.addUser(c, await c.req.json());
-	return c.json(result.ok());
-});
-
-app.post('/public/account/add', async (c) => {
-	const params = await c.req.json();
-	const { userId } = params;
-	const account = await accountService.add(c, params, userId);
-	return c.json(result.ok(account));
-});
-
-app.delete('/public/account/delete', async (c) => {
-	const params = c.req.query();
-	const { userId } = params;
-	await accountService.delete(c, params, userId);
 	return c.json(result.ok());
 });
